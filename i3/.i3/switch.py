d1ff7e3a65da606068316326e7a037b179e28f1d
Zhao <x.zhao.y@gmail.com>
2017-01-26 22:21:15 -0800

    i3: add keybind to swap focus between 2 monitors.

--- i3/.i3/switch.py
+++ i3/.i3/switch.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python2.7
+# Swaps the workspaces active on the 2 monitors.
+# Needs i3-py. Install with 'pip install i3-py'
 
 import i3
+import sys
+
+switch = len(sys.argv) > 1 and sys.argv[1] == 'switch'
 
 # Remember the currently focused window.
 focused_window = i3.filter(nodes=[], focused=True)[0]
@@ -9,13 +14,16 @@ focused_window = i3.filter(nodes=[], focused=True)[0]
 outputs = filter(lambda output: output['active'], i3.get_outputs())
 outputs = [o for o in outputs]
 
-# Set current workspace to output 0 and move it to right, with wrapping.
-i3.workspace(outputs[0]['current_workspace'])
-i3.command('move', 'workspace to output right')
+if switch:
+  for output in outputs:
+    i3.workspace(output['current_workspace'])
+    i3.command('move', 'workspace to output right')
 
-# Set current workspace to output 1 and move it to right, with wrapping.
-i3.workspace(outputs[1]['current_workspace'])
-i3.command('move', 'workspace to output right')
-
-# Restore focus.
-i3.focus(con_id=focused_window['id'])
+  # Restore focused window.
+  i3.focus(con_id=focused_window['id'])
+else:
+  # Just swap focus, not the workspaces.
+  for i, output in enumerate(outputs):
+    if focused_window['output'] == output['name']:
+      i3.workspace(outputs[1 - i]['current_workspace'])
+      break
