4459ec2e664e552e3b5b783a29ebdbe0ed841ae0
Zhao <x.zhao.y@gmail.com>
2017-01-26 21:42:05 -0800

    i3: keep the last focused window after swapping screens.

--- i3/.i3/switch.py
+++ i3/.i3/switch.py
@@ -2,6 +2,9 @@
 
 import i3
 
+# Remember the currently focused window.
+focused_window = i3.filter(nodes=[], focused=True)[0]
+
 # Retrieve workspaces on both monitors.
 outputs = filter(lambda output: output['active'], i3.get_outputs())
 outputs = [o for o in outputs]
@@ -13,3 +16,6 @@ i3.command('move', 'workspace to output right')
 # Set current workspace to output 1 and move it to right, with wrapping.
 i3.workspace(outputs[1]['current_workspace'])
 i3.command('move', 'workspace to output right')
+
+# Restore focus.
+i3.focus(con_id=focused_window['id'])
