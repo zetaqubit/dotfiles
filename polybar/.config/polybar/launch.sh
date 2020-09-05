6d37368f49a8ea1d962c624567dd6cf4e909128e
Zhao Xu <x.zhao.y@gmail.com>
2020-09-05 05:38:35 -0700

    polybar: add option for laptops.

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,7 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar home0 &
-polybar home1 &
+on_laptop=false
+
+if [ "$on_laptop" = false ]; then
+  # For dual-monitors
+  polybar home0 &
+  polybar home1 &
+else
+  # For laptop
+  polybar laptop &
+fi
 
 echo "Polybar launched..."
