e4dd3ea6a2a342ff512a7c52556f36170788664d
Zhao <zeta@zeta.local>
2024-12-16 11:23:01 -0800

    nvim: disable syntax check in vscode.

--- nvim/.config/nvim/lua/z/lazy.lua
+++ nvim/.config/nvim/lua/z/lazy.lua
@@ -11,12 +11,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "z.plugins" }, { import = "z.plugins.lsp" } }, {
-  checker = {
-    enabled = true,
-    notify = false,
+require("lazy").setup(
+  {
+    { import = "z.plugins", cond = not vim.g.vscode },
+    { import = "z.plugins.lsp", cond = false }
   },
-  change_detection = {
-    notify = false,
-  },
-})
+  {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  }
+)
