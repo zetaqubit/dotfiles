43fa3e3902b6ecc31434eeefa4213e0d53c4312c
Zhao <x.zhao.y@gmail.com>
2025-04-27 11:35:03 -0700

    aerospace: fix toggle

--- aerospace/.config/aerospace/toggle_focus_1_and_3.sh
+++ aerospace/.config/aerospace/toggle_focus_1_and_3.sh
@@ -1,3 +1,7 @@
 curr_monitor_id=$(aerospace list-monitors --focused --format %{monitor-id})
-new_monitor_id=$((4 - $curr_monitor_id))  # toggles between 1 and 3
+if [[ "$curr_monitor_id" -eq 1 || "$curr_monitor_id" -eq 3 ]]; then
+  new_monitor_id=$((4 - $curr_monitor_id))  # toggles between 1 and 3
+else
+  new_monitor_id=1
+fi
 aerospace focus-monitor $new_monitor_id
