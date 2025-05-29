d4cf72ca1aca27b63461c2785bc45f91d57b2025
Zhao <x.zhao.y@gmail.com>
2025-05-29 09:33:14 -0700

    nvim: try out noice. keep disabled

---
+++ nvim/.config/nvim/lua/z/plugins/disabled/noice.lua
@@ -0,0 +1,54 @@
+return {
+	"folke/noice.nvim",
+  tag = "stable",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		-- "rcarriga/nvim-notify",
+	},
+	config = function()
+		require("noice").setup({
+      cmdline = {
+        enabled = false,
+      },
+      messages = {
+        enabled = false,
+      },
+      popupmenu = {
+        enabled = false,
+      },
+      notify = {
+        enabled = false,
+      },
+			-- General presets
+			presets = {
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = false, -- position the cmdline and popupmenu together
+				long_message_to_split = false, -- long messages will be sent to a split
+        cmdline_output_to_split = false, -- cmd line 
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+				hover = {
+					silent = true,
+				},
+				progress = {
+					enabled = false,
+				},
+			},
+			-- add any options here
+			routes = {},
+		})
+	end,
+}
