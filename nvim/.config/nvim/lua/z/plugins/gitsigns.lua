fbf26de824f4b9e5e549874d6c1fd6551d3b0948
Zhao <x.zhao.y@gmail.com>
2025-05-28 17:45:20 -0700

    nvim: gitsigns clearer symbols

--- nvim/.config/nvim/lua/z/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/z/plugins/gitsigns.lua
@@ -2,6 +2,21 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    signs_staged = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
 
