c2ea720ae2ddbc8077ec49c86a68c326f38f8e18
Zhao <x.zhao.y@gmail.com>
2025-06-03 06:35:07 -0700

    nvim: assign keymap namespaces

--- nvim/.config/nvim/lua/z/core/keymaps.lua
+++ nvim/.config/nvim/lua/z/core/keymaps.lua
@@ -68,10 +68,10 @@ map("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+map("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+map("n", "<leader>tq", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
